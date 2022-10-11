038821c112fe2380c08f892f3bc68d42c538d440
DurgeshPandit11 <115443660+DurgeshPandit11@users.noreply.github.com>
2022-10-11 13:07:25 +0530

    Update task2.cpp

--- task2.cpp
+++ task2.cpp
@@ -7,17 +7,20 @@ int main()
     int n, sum_a = 0, sum_b = 0, extra;
     cin >> a >> b;
     
+    // Calculating sum of ASCII values of string a
     for(int i = 0; i < a.length(); i++)
     {
         sum_a = sum_a + int(a[i]);
     }
-    
+    // Calculating sum of ASCII values of string b
     for(int i = 0; i < b.length(); i++)
     {
         sum_b = sum_b + int(b[i]);
     }
    
+    // Getting their difference to get ASCII value of extra character in b
     extra = sum_b - sum_a;
+    
     cout << char(extra);
     
     return 0;
